5d249181e7808fa117dbf88ac50e62723c23d28d
Logan Cooper <14ercooper@gmail.com>
2019-05-12 15:20:48 -0600

    Setup basic project structure, POM file

---
+++ README.md
@@ -0,0 +1,6 @@
+14erEdit
+===
+
+A Minecraft world-editing plugin featuring native 1.14 support and lots of power to the user.
+
+Current version: 0.0.1-SNAPSHOT (in development)
---
+++ src/main/java/fourteener/worldeditor/main/Main.java
@@ -0,0 +1,15 @@
+package fourteener.worldeditor.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	@Override
+	public void onEnable () {
+		
+	}
+	
+	@Override
+	public void onDisable () {
+		
+	}
+}
