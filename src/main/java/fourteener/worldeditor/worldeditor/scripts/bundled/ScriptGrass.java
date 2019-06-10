381a1488aa68d71576d7c0be2af32f4b7fd9b703
NotQuiteAmish <calebhostetler@yahoo.com>
2019-06-09 21:33:14 -0400

    Create ScriptGrass

---
+++ src/main/java/fourteener/worldeditor/worldeditor/scripts/bundled/ScriptGrass.java
@@ -0,0 +1,15 @@
+package fourteener.worldeditor.worldeditor.scripts.bundled;
+
+import fourteener.worldeditor.worldeditor.scripts.Craftscript;
+import org.bukkit.block.BlockState;
+import org.bukkit.entity.Player;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ScriptGrass extends Craftscript {
+    @Override
+    public List<BlockState> perform(LinkedList<String> args, Player player) {
+        return null;
+    }
+}
