f1de4b93f9da9ba30223baa4f5125aea3c396cc4
BuildTools <14ercooper@gmail.com>
2020-03-18 16:33:24 -0600

    Add info command

--- src/main/java/com/fourteener/worldeditor/main/Main.java
+++ src/main/java/com/fourteener/worldeditor/main/Main.java
@@ -23,6 +23,7 @@ public class Main extends JavaPlugin {
 		this.getCommand("run").setExecutor(new CommandRun());
 		this.getCommand("runat").setExecutor(new CommandRunat());
 		this.getCommand("debug").setExecutor(new CommandDebug());
+		this.getCommand("info").setExecutor(new CommandInfo());
 		
 		// Register listeners for brushes and wands
 		getServer().getPluginManager().registerEvents(new SelectionWandListener(), this);
