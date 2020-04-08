154e96694c1b02029d74ae4303bc94f0c8d2cd02
14ercooper@gmail.com <14ercooper@gmail.com>
2020-04-08 08:53:04 -0600

    Add large edit confirmation messages

--- src/main/java/com/fourteener/worldeditor/async/AsyncManager.java
+++ src/main/java/com/fourteener/worldeditor/async/AsyncManager.java
@@ -5,20 +5,26 @@ import java.util.Queue;
 
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitScheduler;
 
 import com.fourteener.worldeditor.main.GlobalVars;
 import com.fourteener.worldeditor.main.Main;
+import com.fourteener.worldeditor.operations.Operator;
+import com.fourteener.worldeditor.undo.Undo;
 import com.fourteener.worldeditor.undo.UndoManager;
 
 public class AsyncManager {
 	
 	// How heavy of a load should the async place on the server?
-	long delayTicks = 4;
-	long blockOperations = 10000;
+	public static final long delayTicks = 4;
+	public static final long blockOperations = 10000;
 	
 	// Store operations
 	private Queue<AsyncOperation> operations = new ArrayDeque<AsyncOperation>();
+	
+	// Store large operations
+	private Queue<AsyncOperation> largeOps = new ArrayDeque<AsyncOperation>();
 	
 	// On initialization start the scheduler
 	public AsyncManager () {
@@ -29,6 +35,39 @@ public class AsyncManager {
 				GlobalVars.asyncManager.performOperation();
 			}
 		} , delayTicks, delayTicks);
+	}
+	
+	// Schedule a new task cleanly
+	public void scheduleEdit (Operator o, Player p, ArrayDeque<Block> b) {
+		if (b.size() > Undo.maxBlocks) {
+			largeOps.add(new AsyncOperation(o, p, b));
+			p.sendMessage("§aThis is a large edit that cannot be undone.");
+			p.sendMessage("§aPlease type §b/confirm §ato confirm or §b/cancel §a to cancel");
+			
+		}
+		else {
+			operations.add(new AsyncOperation(true, p));
+			operations.add(new AsyncOperation(o, p, b));
+			operations.add(new AsyncOperation(false, p));
+		}
+	}
+	
+	// Confirm large edits
+	public void confirmEdits (int number) {
+		number = number < 1 ? 1 : number > largeOps.size() ? largeOps.size() : number;
+		if (number < largeOps.size()) return;
+		while (number-- > 0) {
+			operations.add(largeOps.remove());
+		}
+	}
+	
+	// Cancel large edits
+	public void cancelEdits (int number) {
+		number = number < 1 ? 1 : number > largeOps.size() ? largeOps.size() : number;
+		if (number < largeOps.size()) return;
+		while (number-- > 0) {
+			largeOps.remove();
+		}
 	}
 	
 	// Scheduled task to operate
