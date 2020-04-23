b97378636544893e8239081c3a293ceae1b16495
14ercooper@gmail.com <14ercooper@gmail.com>
2020-04-23 13:43:30 -0600

    Prototype schemlite object format

---
+++ src/main/java/com/fourteener/schematics/SchemLite.java
@@ -0,0 +1,31 @@
+package com.fourteener.schematics;
+
+public class SchemLite {
+	
+	// The size of the schem lite
+	int xSize, ySize, zSize;
+	// Offset (used by clipboard)
+	int xOff, yOff, zOff;
+	// Where this is on disk; within the 14erEdit folder
+	String outPath;
+	// Creator details
+	String authorName, creationDate;
+	
+	// Create a new schem lite object
+	
+	// Get a block iterator corresponding with this schem lite
+	
+	// Reset the write position of the schem lite object
+	
+	// Add a block to the schem lite object
+	
+	// Reset the read position to the start of the object
+	
+	// Get the next block in this schem lite
+	
+	// Move a schem lite object's disk reference
+	
+	// Clone the schem lite's data to a persistent file
+	
+	// Load the schem lite's data from a persistent file
+}
