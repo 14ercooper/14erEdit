cedb7feb59d7c6bb11686e78468d1bf4b8bc20d6
14ercooper@gmail.com <14ercooper@gmail.com>
2020-07-18 05:11:07 -0600

    Add absolute coordinate capability to at node

--- src/main/java/com/_14ercooper/worldeditor/operations/operators/query/BlockAtNode.java
+++ src/main/java/com/_14ercooper/worldeditor/operations/operators/query/BlockAtNode.java
@@ -7,20 +7,28 @@ import com._14ercooper.worldeditor.main.Main;
 import com._14ercooper.worldeditor.main.NBTExtractor;
 import com._14ercooper.worldeditor.operations.Operator;
 import com._14ercooper.worldeditor.operations.operators.Node;
-import com._14ercooper.worldeditor.operations.operators.core.NumberNode;
+import com._14ercooper.worldeditor.operations.operators.core.StringNode;
 import com._14ercooper.worldeditor.operations.operators.world.BlockNode;
 
 public class BlockAtNode extends BlockNode {
 
-	NumberNode x, y, z;
+	StringNode x, y, z;
+	int xV, yV, zV;
+	boolean xA = false, yA = false, zA = false;
 	Node node;
 
 	public BlockAtNode newNode() {
 		BlockAtNode baNode = new BlockAtNode();
 		try {
-			baNode.x = GlobalVars.operationParser.parseNumberNode();
-			baNode.y = GlobalVars.operationParser.parseNumberNode();
-			baNode.z = GlobalVars.operationParser.parseNumberNode();
+			baNode.x = GlobalVars.operationParser.parseStringNode();
+			baNode.y = GlobalVars.operationParser.parseStringNode();
+			baNode.z = GlobalVars.operationParser.parseStringNode();
+			if (x.contents.contains("a")) xA = true;
+			if (y.contents.contains("a")) yA = true;
+			if (z.contents.contains("a")) zA = true;
+			xV = Integer.parseInt(x.contents.replaceAll("[^0-9]", ""));
+			yV = Integer.parseInt(y.contents.replaceAll("[^0-9]", ""));
+			zV = Integer.parseInt(z.contents.replaceAll("[^0-9]", ""));
 			try {
 				baNode.node = GlobalVars.operationParser.parsePart();
 			} catch (Exception e) {
@@ -39,7 +47,10 @@ public class BlockAtNode extends BlockNode {
 	public boolean performNode() {
 		try {
 			Block currBlock = Operator.currentBlock;
-			Operator.currentBlock = Operator.currentPlayer.getWorld().getBlockAt((int) x.getValue() + currBlock.getX(), (int) y.getValue() + currBlock.getY(), (int) z.getValue() + currBlock.getZ());
+			Operator.currentBlock = Operator.currentPlayer.getWorld().getBlockAt(
+					xV + (xA ? 0 : currBlock.getX()),
+					yV + (yA ? 0 : currBlock.getY()),
+					zV + (zA ? 0 : currBlock.getZ()));
 			boolean matches = node.performNode();
 			Operator.currentBlock = currBlock;
 			return matches;
@@ -51,18 +62,27 @@ public class BlockAtNode extends BlockNode {
 	
 	// Return the material this node references
 	public String getBlock () {
-		return Operator.currentBlock.getRelative((int) x.getValue(), (int) y.getValue(), (int) z.getValue()).getType().toString();
+		return Operator.currentPlayer.getWorld().getBlockAt(
+				xV + (xA ? 0 : Operator.currentBlock.getX()),
+				yV + (yA ? 0 : Operator.currentBlock.getY()),
+				zV + (zA ? 0 : Operator.currentBlock.getZ())).toString();
 	}
 
 	// Get the data of this block
 	public String getData () {
-		return Operator.currentBlock.getRelative((int) x.getValue(), (int) y.getValue(), (int) z.getValue()).getBlockData().getAsString();
+		return Operator.currentPlayer.getWorld().getBlockAt(
+				xV + (xA ? 0 : Operator.currentBlock.getX()),
+				yV + (yA ? 0 : Operator.currentBlock.getY()),
+				zV + (zA ? 0 : Operator.currentBlock.getZ())).toString();
 	}
 	
 	// Get the NBT of this block
 	public String getNBT() {
 		NBTExtractor nbt = new NBTExtractor();
-		return nbt.getNBT(Operator.currentBlock.getRelative((int) x.getValue(), (int) y.getValue(), (int) z.getValue()));
+		return nbt.getNBT(Operator.currentPlayer.getWorld().getBlockAt(
+				xV + (xA ? 0 : Operator.currentBlock.getX()),
+				yV + (yA ? 0 : Operator.currentBlock.getY()),
+				zV + (zA ? 0 : Operator.currentBlock.getZ())));
 	}
 
 	public int getArgCount() {
