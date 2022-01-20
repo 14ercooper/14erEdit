92b8321ac383074ac372ea877041327b8295a7d8
Logan Cooper <14ercooper@gmail.com>
2022-01-20 06:29:21 -0700

    Correctly fix the GPL header

--- src/main/java/com/_14ercooper/worldeditor/operations/operators/fun/SmallRuinNode.java
+++ src/main/java/com/_14ercooper/worldeditor/operations/operators/fun/SmallRuinNode.java
@@ -1,6 +1,19 @@
-// Licensed Under GPL v3.0
-//https://github.com/14ercooper/14erEdit
-
+/**
+ * This file is part of 14erEdit.
+ * 
+  * 14erEdit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * 14erEdit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with 14erEdit.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package com._14ercooper.worldeditor.operations.operators.fun;
 
